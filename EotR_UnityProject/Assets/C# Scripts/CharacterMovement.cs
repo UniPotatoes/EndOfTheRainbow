2a8dca9757f33865dd62d8a427161eaf0ee82e14
Aramilion <aramil.anakir@onet.pl>
2015-11-28 14:28:37 +0100

    Fixed jumping, and pivot placement

--- EotR_UnityProject/Assets/C# Scripts/CharacterMovement.cs
+++ EotR_UnityProject/Assets/C# Scripts/CharacterMovement.cs
@@ -8,18 +8,18 @@ public class CharacterMovement : MonoBehaviour
     
     //wall checking
     public Transform wallCheckUR, wallCheckBR, wallCheckUL, wallCheckBL;
+	public Transform PivotPoint;
     [SerializeField]private bool walled;
 
     //ground checking
-    public Transform groundCheck; //groundCheck object
     public LayerMask whatIsGround; //define what is treated like ground
-    float groundRadius = 0.1f;
+    float groundRadius = 0.05f;
     [SerializeField]private bool grounded = false; //is it on the ground
 
     //jumping
     public int jumpForce = 2; //jump modifier
     public int maxJumpForce = 700; //maximum jump force (not working now)
-    public float mouseX, mouseY; //mouse coordinates on screen
+    float mouseX, mouseY; //mouse coordinates on screen
     float jumpNowY;
     float jumpNowX;
     private Rigidbody2D rigidBody; //rigidbody which will jump
@@ -38,6 +38,12 @@ public class CharacterMovement : MonoBehaviour
 
 	void Update ()
     {
+		mouseX = Input.mousePosition.x; //mouse horizontal position on screen
+		mouseY = Input.mousePosition.y; //mouse vertical position on screen
+		relativeMouseX = mouseX - Screen.width / 2; //mouse horizontal position relative to center of the screen
+		relativeMouseY = mouseY - Screen.height / 2; //mouse vertical position relative to center of the screen
+
+
         if (grounded && Input.GetKeyDown (KeyCode.Mouse0)) //if it is on the ground then jump when key is pressed
         {
             anim.SetTrigger("jumped");
@@ -87,27 +93,18 @@ public class CharacterMovement : MonoBehaviour
     {
 		//checks if GroundCheck object is colliding with something
 		grounded = CheckForGround ();
-		if (grounded) //Horizontal movement
+		if (grounded /*&& anim.GetBool("onLand") == true*/) //Horizontal movement
 		{
 			Move();
 		}
 
 		walled = CheckForWall ();
-		/*
-		//checks if any of WallCheck objects is colliding with something
-
-		if (walled)
-        {
-			walled = false;
-			rigidBody.velocity = new Vector2(-rigidBody.velocity.x, rigidBody.velocity.y);
-        }
-        */
     }
 
     bool CheckForGround ()
 	{
 		Vector2 positionBL = wallCheckBL.position;
-		Vector2 positionBR = new Vector2 (wallCheckBR.position.x, wallCheckBR.position.y - 0.1f);
+		Vector2 positionBR = new Vector2 (wallCheckBR.position.x, wallCheckBR.position.y - groundRadius);
 		if (Physics2D.OverlapArea (positionBL, positionBR, whatIsGround) != null) 
 		{
 			return true;
@@ -144,21 +141,30 @@ public class CharacterMovement : MonoBehaviour
         mouseY = Input.mousePosition.y; //mouse vertical position on screen
         relativeMouseX = mouseX - Screen.width / 2; //mouse horizontal position relative to center of the screen
         relativeMouseY = mouseY - Screen.height / 2; //mouse vertical position relative to center of the screen
-        float jumpX = relativeMouseX - transform.position.x; //horizontal force modifiier to be applied to object
-        float jumpY = relativeMouseY - transform.position.y; //vertical force modifiier to be applied to object
-        jumpNowX = jumpForce * jumpX; //final horizontal jump force
-        jumpNowY = jumpForce * jumpY; //final vertical jump force
-        if (relativeMouseY > transform.position.y) //don't jump down, it's impossible
+		//float jumpX = relativeMouseX - PivotPoint.transform.position.x; //horizontal force modifiier to be applied to object
+		//float jumpY = relativeMouseY - PivotPoint.transform.position.y; //vertical force modifiier to be applied to object
+		float jumpX = relativeMouseX;
+		float jumpY = relativeMouseY;
+		jumpNowX = jumpForce * jumpX * 0.1f; //final horizontal jump force
+		jumpNowY = jumpForce * jumpY * 0.1f; //final vertical jump force
+		if (relativeMouseY > PivotPoint.transform.position.y + 5.0f) //don't jump down, it's impossible
         {
             Vector2 jumping = new Vector2 (jumpNowX, jumpNowY);
-            rigidBody.AddForce(jumping, ForceMode2D.Force);
+            rigidBody.AddForce(jumping, ForceMode2D.Impulse);
         }
     }
 
     void Move()
     {
-        float move = Input.GetAxis("Horizontal"); //poruszanie się w poziomie
-        rigidBody.velocity = new Vector2(move * horizontalSpeed, rigidBody.velocity.y);
+		float move;
+		if (rigidBody.velocity.y == 0) {
+			move = Input.GetAxis ("Horizontal") * horizontalSpeed; //poruszanie się w poziomie
+		} 
+		else 
+		{
+			move = rigidBody.velocity.x;
+		}
+        rigidBody.velocity = new Vector2(move, rigidBody.velocity.y);
     }
 
     void Flip()
---
+++ EotR_UnityProject/Assets/C# Scripts/DEBUG.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DEBUG : MonoBehaviour {
+
+	public Transform PivotPoint;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.position = new Vector2 (PivotPoint.transform.position.x, PivotPoint.transform.position.y);
+	}
+}
