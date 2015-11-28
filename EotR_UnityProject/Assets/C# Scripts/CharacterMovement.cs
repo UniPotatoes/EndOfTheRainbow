04c9927961a8c0d1d289e68e7271506b0c596334
Aramilion <aramil.anakir@onet.pl>
2015-11-28 19:09:57 +0100

    Super Secret Update :3

--- EotR_UnityProject/Assets/C# Scripts/CharacterMovement.cs
+++ EotR_UnityProject/Assets/C# Scripts/CharacterMovement.cs
@@ -3,6 +3,12 @@ using System.Collections;
 
 public class CharacterMovement : MonoBehaviour
 {
+
+	public GameObject JumpingArcDot;
+	float interval = 0.01f; 
+	float nextTime = 0;
+	
+	//-----------------
     //moving
     public float horizontalSpeed = 10f; //horizontal movement speed
     
@@ -17,7 +23,7 @@ public class CharacterMovement : MonoBehaviour
     [SerializeField]private bool grounded = false; //is it on the ground
 
     //jumping
-    public int jumpForce = 2; //jump modifier
+    public int jumpForce = 1; //jump modifier
     public int maxJumpForce = 700; //maximum jump force (not working now)
     float mouseX, mouseY; //mouse coordinates on screen
     float jumpNowY;
@@ -38,6 +44,15 @@ public class CharacterMovement : MonoBehaviour
 
 	void Update ()
     {
+		if(Input.GetKey((KeyCode.Space)))
+		if (Time.time >= nextTime ) {
+			nextTime += interval; 
+			Debug.Log(nextTime);
+
+			SpawnJumpingArcDot ();
+		}
+
+
 		mouseX = Input.mousePosition.x; //mouse horizontal position on screen
 		mouseY = Input.mousePosition.y; //mouse vertical position on screen
 		relativeMouseX = mouseX - Screen.width / 2; //mouse horizontal position relative to center of the screen
@@ -143,8 +158,33 @@ public class CharacterMovement : MonoBehaviour
         relativeMouseY = mouseY - Screen.height / 2; //mouse vertical position relative to center of the screen
 		//float jumpX = relativeMouseX - PivotPoint.transform.position.x; //horizontal force modifiier to be applied to object
 		//float jumpY = relativeMouseY - PivotPoint.transform.position.y; //vertical force modifiier to be applied to object
-		float jumpX = relativeMouseX;
-		float jumpY = relativeMouseY;
+		float jumpX;
+		if (relativeMouseX < -150.0f) 
+		{
+			jumpX = -150.0f;
+		} 
+		else 
+		{
+			if (relativeMouseX > 150.0f) 
+			{
+				jumpX = 150.0f;
+			}
+			else
+			{
+			jumpX = relativeMouseX;
+			}
+		}
+
+		float jumpY;
+		if (relativeMouseY > 200.0f) 
+		{
+			jumpY = 200.0f;
+		} 
+		else 
+		{
+			jumpY = relativeMouseY;
+		}
+
 		jumpNowX = jumpForce * jumpX * 0.1f; //final horizontal jump force
 		jumpNowY = jumpForce * jumpY * 0.1f; //final vertical jump force
 		if (relativeMouseY > PivotPoint.transform.position.y + 5.0f) //don't jump down, it's impossible
@@ -174,4 +214,29 @@ public class CharacterMovement : MonoBehaviour
         theScale.x *= -1;
         transform.localScale = theScale; 
     }
+
+	void SpawnJumpingArcDot ()
+	{
+		Vector3 InstantiatePoint = new Vector3 (PivotPoint.transform.position.x, PivotPoint.transform.position.y, PivotPoint.transform.position.z);
+		Instantiate (JumpingArcDot, InstantiatePoint, Quaternion.identity);
+
+	}
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ EotR_UnityProject/Assets/C# Scripts/JumpingArcPathDisplay.cs
@@ -0,0 +1,60 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class JumpingArcPathDisplay : MonoBehaviour {
+
+	float mouseX, mouseY;
+	[SerializeField]private float relativeMouseX, relativeMouseY;
+	public int jumpForce = 1; //jump modifier
+	public Transform PivotPoint;
+
+	float jumpNowX, jumpNowY;
+	private Rigidbody2D rigidBody;
+	// Use this for initialization
+	void Awake () {
+		rigidBody = GetComponent<Rigidbody2D>();
+		PivotPoint = GameObject.Find ("PivotPoint").transform;
+
+
+
+		//stuff needed for jumping
+		mouseX = Input.mousePosition.x; //mouse horizontal position on screen
+		mouseY = Input.mousePosition.y; //mouse vertical position on screen
+		relativeMouseX = mouseX - Screen.width / 2; //mouse horizontal position relative to center of the screen
+		relativeMouseY = mouseY - Screen.height / 2; //mouse vertical position relative to center of the screen
+		//float jumpX = relativeMouseX - PivotPoint.transform.position.x; //horizontal force modifiier to be applied to object
+		//float jumpY = relativeMouseY - PivotPoint.transform.position.y; //vertical force modifiier to be applied to object
+		float jumpX;
+		if (relativeMouseX < -150.0f) {
+			jumpX = -150.0f;
+		} else {
+			if (relativeMouseX > 150.0f) {
+				jumpX = 150.0f;
+			} else {
+				jumpX = relativeMouseX;
+			}
+		}
+		
+		float jumpY;
+		if (relativeMouseY > 200.0f) {
+			jumpY = 200.0f;
+		} else {
+			jumpY = relativeMouseY;
+		}
+		
+		jumpNowX = jumpForce * jumpX * 0.1f; //final horizontal jump force
+		jumpNowY = jumpForce * jumpY * 0.1f; //final vertical jump force
+		if (relativeMouseY > PivotPoint.transform.position.y + 5.0f) { //don't jump down, it's impossible
+			Vector2 jumping = new Vector2 (jumpNowX, jumpNowY);
+			rigidBody.AddForce (jumping, ForceMode2D.Impulse);
+		}
+	}
+	void OnCollisionEnter2D(Collision2D col)
+	{
+		if (col.gameObject.layer == 8 ){
+			Debug.Log ("I've hit somethin");
+			Destroy (gameObject);
+		}
+	}
+
+}
