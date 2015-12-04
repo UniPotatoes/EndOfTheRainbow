3691c44ca6f58a99dcb7e3ca9fdc8a8b1a2dcd7a
Casiell <wojtekpojda@wp.pl>
2015-12-04 17:06:30 +0100

    Displaying Tips
    Added scripts and object which display tips on screen

---
+++ EotR_UnityProject/Assets/C# Scripts/DisplayingTips.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class DisplayingTips : MonoBehaviour {
+
+    [SerializeField]private bool showTip = true;
+    public Text tip;
+
+    void DisplayTip(string tipText)
+    {
+        showTip = true;
+        tip.text = tipText;
+        tip.enabled = true;
+
+    }
+    void DisableTip()
+    {
+        showTip = false;
+        tip.text = "disabled";
+        tip.enabled = false;
+    }
+}
---
+++ EotR_UnityProject/Assets/C# Scripts/TipEnabler.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TipEnabler : MonoBehaviour {
+    public GameObject UIText;
+    public string tipContent;
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        //Debug.Log("Object entered the trigger.");
+        UIText.SendMessage("DisplayTip", tipContent);
+    }
+    void OnTriggerExit2D(Collider2D other)
+    {
+        UIText.SendMessage("DisableTip");
+    }
+}
