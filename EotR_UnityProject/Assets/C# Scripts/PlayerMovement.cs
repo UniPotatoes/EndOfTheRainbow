fe2fb22e5cdd250a25af92fa337aaa9b35e1c8d2
Aramilion <aramil.anakir@onet.pl>
2015-12-06 15:07:25 +0100

    New CharacterScript

--- EotR_UnityProject/Assets/C# Scripts/PlayerMovement.cs
+++ EotR_UnityProject/Assets/C# Scripts/PlayerMovement.cs
@@ -3,12 +3,6 @@ using System.Collections;
 
 public class PlayerMovement : MonoBehaviour
 {
-	
-	public GameObject JumpingArcDot;
-	float interval = 0.01f; 
-	float nextTime = 0;
-	
-	//-----------------
 	//moving
 	public float horizontalSpeed = 10f; //horizontal movement speed
 	
@@ -23,13 +17,10 @@ public class PlayerMovement : MonoBehaviour
 	[SerializeField]private bool grounded = false; //is it on the ground
 	
 	//jumping
-	public int jumpForce = 1; //jump modifier
-	public int maxJumpForce = 700; //maximum jump force (not working now)
-	float mouseX, mouseY; //mouse coordinates on screen
-	float jumpNowY;
-	float jumpNowX;
+	private Vector2 jumpingForce;
+	public float jumpForce = 1.0f; //jump modifier
+	public float jumpModifier = 5.0f;
 	private Rigidbody2D rigidBody; //rigidbody which will jump
-	[SerializeField]private float relativeMouseX, relativeMouseY; //mouse coordinates relative to the object
 	
 	//facing
 	public bool facingRight = true;
@@ -43,30 +34,29 @@ public class PlayerMovement : MonoBehaviour
 	}
 	
 	void Update ()
-	{
-		if(Input.GetKey((KeyCode.Space)))
-		if (Time.time >= nextTime ) {
-			nextTime += interval; 
-			Debug.Log(nextTime);
-			
-			SpawnJumpingArcDot ();
-		}
-		
-		
-		mouseX = Input.mousePosition.x; //mouse horizontal position on screen
-		mouseY = Input.mousePosition.y; //mouse vertical position on screen
-		relativeMouseX = mouseX - Screen.width / 2; //mouse horizontal position relative to center of the screen
-		relativeMouseY = mouseY - Screen.height / 2; //mouse vertical position relative to center of the screen
-		
-		
-		if (grounded && Input.GetKeyDown (KeyCode.Mouse0)) //if it is on the ground then jump when key is pressed
+	{				
+		if (grounded && Input.GetKeyDown(KeyCode.W)) //if it is on the ground then jump when key is pressed
 		{
 			anim.SetTrigger("jumped");
 			Jump();
 		}
-		
+		/*
+		if (Input.GetKey (KeyCode.W)) 
+		{
+			if(jumpingForce.y > 0.0f)
+			{
+				jumpingForce.y += jumpModifier * Time.deltaTime;
+			}
+		}
+
+		if (Input.GetKeyUp (KeyCode.W)) 
+		{
+			jumpForce = 0.0f;
+		}
+*/
 		if (grounded)
 		{
+	//		jumpingForce.y = 0.0f;
 			anim.SetBool ("onLand", true);
 		}
 		else
@@ -104,15 +94,13 @@ public class PlayerMovement : MonoBehaviour
 			}
 		}		
 	}
+
 	void FixedUpdate()
 	{
 		//checks if GroundCheck object is colliding with something
 		grounded = CheckForGround ();
-		if (grounded /*&& anim.GetBool("onLand") == true*/) //Horizontal movement
-		{
-			Move();
-		}
-		
+
+		Move();
 		walled = CheckForWall ();
 	}
 	
@@ -151,47 +139,9 @@ public class PlayerMovement : MonoBehaviour
 	
 	void Jump()
 	{
-		//stuff needed for jumping
-		mouseX = Input.mousePosition.x; //mouse horizontal position on screen
-		mouseY = Input.mousePosition.y; //mouse vertical position on screen
-		relativeMouseX = mouseX - Screen.width / 2; //mouse horizontal position relative to center of the screen
-		relativeMouseY = mouseY - Screen.height / 2; //mouse vertical position relative to center of the screen
-		//float jumpX = relativeMouseX - PivotPoint.transform.position.x; //horizontal force modifiier to be applied to object
-		//float jumpY = relativeMouseY - PivotPoint.transform.position.y; //vertical force modifiier to be applied to object
-		float jumpX;
-		if (relativeMouseX < -150.0f) 
-		{
-			jumpX = -150.0f;
-		} 
-		else 
-		{
-			if (relativeMouseX > 150.0f) 
-			{
-				jumpX = 150.0f;
-			}
-			else
-			{
-				jumpX = relativeMouseX;
-			}
-		}
-		
-		float jumpY;
-		if (relativeMouseY > 200.0f) 
-		{
-			jumpY = 200.0f;
-		} 
-		else 
-		{
-			jumpY = relativeMouseY;
-		}
-		
-		jumpNowX = jumpForce * jumpX * 0.1f; //final horizontal jump force
-		jumpNowY = jumpForce * jumpY * 0.1f; //final vertical jump force
-		if (relativeMouseY > PivotPoint.transform.position.y + 5.0f) //don't jump down, it's impossible
-		{
-			Vector2 jumping = new Vector2 (jumpNowX, jumpNowY);
-			rigidBody.AddForce(jumping, ForceMode2D.Impulse);
-		}
+		jumpingForce.x = 0.0f;
+		jumpingForce.y = jumpForce;
+		rigidBody.AddForce(jumpingForce, ForceMode2D.Impulse);
 	}
 	
 	void Move()
@@ -207,13 +157,6 @@ public class PlayerMovement : MonoBehaviour
 		Vector3 theScale = transform.localScale;
 		theScale.x *= -1;
 		transform.localScale = theScale; 
-	}
-	
-	void SpawnJumpingArcDot ()
-	{
-		Vector3 InstantiatePoint = new Vector3 (PivotPoint.transform.position.x, PivotPoint.transform.position.y, PivotPoint.transform.position.z);
-		Instantiate (JumpingArcDot, InstantiatePoint, Quaternion.identity);
-		
 	}
 }
 
