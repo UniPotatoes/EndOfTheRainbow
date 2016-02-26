66afd218faa1604979047d024c5980432f3fe967
Aramilion <aramil.anakir@onet.pl>
2016-02-26 20:28:17 +0100

    Generation to map

---
+++ EotR_UnityProject/Assets/C# Scripts/Map Generator/DungeonGenerator.cs
@@ -0,0 +1,174 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DungeonGenerator : MonoBehaviour
+{
+
+    public int width;
+    public int height;
+    public int smoothSteps = 4;
+
+    public string seed;
+    public bool useRandomSeed;
+
+    [Range(0, 100)]
+    public int randomFillPercent;
+
+    public int[,] map;
+
+    public GameObject Rooms;
+    public GameObject Room_1x1;
+    public GameObject Room_1x2;
+    public GameObject Room_2x1;
+    public GameObject Room_2x2;
+
+   // public GameObject Player;
+
+    void Start()
+    {
+        GenerateMap();
+    }
+
+    void Update()
+    {
+        //r - resetowanie mapy, Debug
+        if (Input.GetKey("r"))
+        {
+            ResetMap();
+        }
+    }
+
+    void ResetMap()
+    {
+        var gameObjects = GameObject.FindGameObjectsWithTag("Room");
+        foreach (var target in gameObjects)
+        {
+            GameObject.Destroy(target);
+        }
+        
+        GenerateMap();
+    }
+
+    void GenerateMap()
+    {
+        map = new int[width, height];
+        RandomFillMap();
+
+        for (int i = 0; i < smoothSteps; i++)
+        {
+            SmoothMap();
+        }
+
+        PlaceRooms();
+
+      //  SpawnPlayer();
+    }
+
+    void RandomFillMap()
+    {
+        if (useRandomSeed)
+        {
+            seed = Time.time.ToString();
+        }
+
+        System.Random pseudoRandom = new System.Random(seed.GetHashCode());
+
+        for (int x = 0; x < width; x++)
+        {
+            for (int y = 0; y < height; y++)
+            {
+                if (x == 0 || x == width - 1 || y == 0 || y == height - 1)
+                {
+                    map[x, y] = 0;
+                }
+                else {
+                    map[x, y] = (pseudoRandom.Next(0, 100) < randomFillPercent) ? 1 : 0;
+                }
+            }
+        }
+    }
+
+    void SmoothMap()
+    {
+        for (int x = 0; x < width; x++)
+        {
+            for (int y = 0; y < height; y++)
+            {
+                int neighbourWallTiles = GetSurroundingWallCount(x, y);
+
+                if (neighbourWallTiles > 4)
+                    map[x, y] = 1;
+                else if (neighbourWallTiles < 4)
+                    map[x, y] = 0;
+
+            }
+        }
+    }
+
+    int GetSurroundingWallCount(int gridX, int gridY)
+    {
+        int wallCount = 0;
+        for (int neighbourX = gridX - 1; neighbourX <= gridX + 1; neighbourX++)
+        {
+            for (int neighbourY = gridY - 1; neighbourY <= gridY + 1; neighbourY++)
+            {
+                if (neighbourX >= 0 && neighbourX < width && neighbourY >= 0 && neighbourY < height)
+                {
+                    if (neighbourX != gridX || neighbourY != gridY)
+                    {
+                        wallCount += map[neighbourX, neighbourY];
+                    }
+                }
+                else {
+                    wallCount++;
+                }
+            }
+        }
+
+        return wallCount;
+    }
+
+    void PlaceRooms()
+    {
+        if (map != null)
+        {
+            for (int x = 0; x < width; x++)
+            {
+                for (int y = 0; y < height; y++)
+                {
+                    if (map[x, y] == 1)
+                    {
+                        Vector3 pos = new Vector3(-width / 2 + x + .5f, -height / 2 + y + .5f, 0);
+                       
+                        GameObject tile = Instantiate(Room_1x1, pos, transform.rotation) as GameObject;
+                        tile.transform.parent = Rooms.transform;        
+                    }
+                }
+            }
+        }
+    }
+    /*
+    void SpawnPlayer()
+    {
+        int x; int y;
+        bool foundPlace = false;
+        for (x = 0; x < width; x++)
+        {
+            for (y = 0; y < height - 1; y++)
+            {
+                if (map[x, y] == 0 && map[x, y - 1] == 0)
+                {
+                    Vector3 pos = new Vector3(-width / 2 + x + .5f, -height / 2 + y, 0);
+                    Instantiate(Player, pos, transform.rotation);
+                    foundPlace = true;
+                    break;
+                }
+            }
+            if (foundPlace == true)
+            {
+                break;
+            }
+        }
+    }
+    */
+}
---
+++ EotR_UnityProject/Assets/C# Scripts/Map Generator/MapDigger.cs
@@ -0,0 +1,155 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MapDigger : MonoBehaviour
+{
+    public int mapWidth;
+    public int mapHeight;
+
+    public RoomType[,] map;
+
+    public GameObject Rooms;
+    public GameObject Room_1x1;
+    public GameObject Room_1x2;
+    public GameObject Room_2x1;
+    public GameObject Room_2x2;
+
+    public int roomsToDig = 1;
+
+    public enum RoomType {None = -1, FreeSpace, Room_1x1, Room_1x2, Room_2x1, Room_2x2};
+
+    public int minWidthOfMap = 0;
+
+    // Use this for initialization
+    void Start()
+    {
+        if(minWidthOfMap == 0)
+        {
+            minWidthOfMap = (int)(mapWidth * 0.5f);
+        }
+        GenerateMap();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //r - resetowanie mapy, Debug
+        if (Input.GetKeyDown("r"))
+        {
+            ResetMap();
+        }
+    }
+
+    void ResetMap()
+    {
+        var gameObjects = GameObject.FindGameObjectsWithTag("Room");
+        foreach (var target in gameObjects)
+        {
+            GameObject.Destroy(target);
+        }
+
+        GenerateMap();
+    }
+
+    void GenerateMap()
+    {
+        InitialiseMap();
+
+        DigPaths();
+
+        PlaceRooms();
+    }
+
+    void InitialiseMap()
+    {
+        map = new RoomType[mapWidth, mapHeight];
+        ResetMapValues();
+    }
+
+    void ResetMapValues()
+    {
+        for(int i = 0; i < mapWidth; i++)
+            for (int j = 0; j < mapHeight; j++)
+                map[i, j] = RoomType.None;
+    }
+
+    enum Direction { North, South, West, East };
+    void DigPaths()
+    {
+        bool mapWideEnough = false;
+        while (!mapWideEnough)
+        {
+            Vector2 diggerPosition = new Vector2(0, mapHeight / 2);
+            ResetMapValues();
+            map[(int)diggerPosition.x, (int)diggerPosition.y] = RoomType.FreeSpace;
+        
+            int roomsDug = 1;
+            while (roomsDug < roomsToDig)
+            {
+                Direction direction = (Direction)Random.Range(0, 4);
+                diggerPosition = MoveDiggerInDirection(diggerPosition, direction);
+                if (map[(int)diggerPosition.x, (int)diggerPosition.y] != RoomType.FreeSpace)
+                {
+                    map[(int)diggerPosition.x, (int)diggerPosition.y] = RoomType.FreeSpace;
+                    roomsDug += 1;
+                }
+            }
+            mapWideEnough = CheckIfMapIsWiderThan(minWidthOfMap);
+        }
+    }
+
+    bool CheckIfMapIsWiderThan(int minWidthOfMap)
+    {
+        int indexOfMostRightRoom = 0;
+        for (int j = 0; j < mapHeight; j++)
+        {
+            for (int i = mapWidth-1; i > 0; i--)
+            {
+                if (map[i, j] == RoomType.FreeSpace)
+                {
+                    indexOfMostRightRoom = i;
+                    break;
+                }
+            }
+            if(indexOfMostRightRoom != 0)
+            {
+                break;
+            }
+        }
+        return indexOfMostRightRoom >= minWidthOfMap;
+    }
+
+    Vector2 MoveDiggerInDirection(Vector2 diggerPosition, Direction direction)
+    {
+        Vector2 diggerMove = Vector2.zero;
+        switch (direction)
+        {
+            case Direction.North: if(diggerPosition.y < mapHeight-1) diggerMove = Vector2.up; break;
+            case Direction.South: if (diggerPosition.y > 0) diggerMove = Vector2.down; break;
+            case Direction.West: if (diggerPosition.x > 0) diggerMove = Vector2.left; break;
+            case Direction.East: if (diggerPosition.x < mapWidth-1) diggerMove = Vector2.right; break;
+        }
+        return diggerPosition += diggerMove;
+    }
+
+    //only placing 1x1
+    void PlaceRooms()
+    {
+        if (map != null)
+        {
+            for (int x = 0; x < mapWidth; x++)
+            {
+                for (int y = 0; y < mapHeight; y++)
+                {
+                    if (map[x, y] == RoomType.FreeSpace)
+                    {
+                        Vector3 pos = new Vector3(-mapWidth / 2 + x + .5f, -mapHeight / 2 + y + .5f, 0);
+
+                        GameObject tile = Instantiate(Room_1x1, pos, transform.rotation) as GameObject;
+                        tile.transform.parent = Rooms.transform;
+                    }
+                }
+            }
+        }
+    }
+}
