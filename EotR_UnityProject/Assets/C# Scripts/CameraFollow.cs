1ae8fb3468938da10d7e21a6de27e465a4680596
Aramilion <aramil.anakir@onet.pl>
2016-03-19 19:07:12 +0100

    camera movement

---
+++ EotR_UnityProject/Assets/C# Scripts/CameraFollow.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraFollow : MonoBehaviour {
+
+    [SerializeField]
+    private Vector2 maxCameraDisplacement;
+    [SerializeField]
+    private Vector2 minCameraPosition;
+    [SerializeField]
+    private Vector2 maxCameraPosition;
+
+    private Transform target;
+
+    void Start()
+    {
+        target = GameObject.Find("Player").transform;
+    } 	
+
+	void LateUpdate ()
+    {
+        transform.position = new Vector3(Mathf.Clamp(target.position.x, minCameraPosition.x, maxCameraPosition.x), 
+                                         Mathf.Clamp(target.position.y, minCameraPosition.y, maxCameraPosition.y), -1);
+
+        
+	}
+}
