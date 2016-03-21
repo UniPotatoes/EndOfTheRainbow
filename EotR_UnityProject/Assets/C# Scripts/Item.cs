19d931e8a9470bb872f1b1596a95d5272084d4f5
Jashminee <jpawelek@onet.pl>
2016-03-21 13:44:16 +0100

    list template

---
+++ EotR_UnityProject/Assets/C# Scripts/Item.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[System.Serializable]
+public class Item
+{
+
+    public string itemName;
+    public int itemID;
+
+    [System.Serializable]
+    public enum ItemType
+    {
+        Mammal,
+        Bird,
+        Fish,
+        Insect,
+        Reptile
+    }
+
+    public Item(string name, int id)
+    {
+        itemName = name;
+        itemID = id;
+
+    }
+
+    public Item()
+    {
+    }
+}
